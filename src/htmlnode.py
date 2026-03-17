f215dcf195bb0af8be7245d390e9cca3347efd6b
Tiago <tiagotbw@gmail.com>
2026-03-16 22:43:49 -0300

    feat: implement HTMLNode class with methods for HTML representation

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("Not implemented!")
+
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        result = []
+        for key, value in self.props.items():
+            result.append(f' {key}="{value}"')
+        return "".join(result)
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
